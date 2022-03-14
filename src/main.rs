5a1c0d37fcf30e9f6696d285282f1781a6ccc3b1
Yan Mingzhi <realraindust@gmail.com>
2022-03-14 17:26:24 +0800

    replace source file if not specify output

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use crate::json_log::JsonLog;
+use chrono::Utc;
 use clap::Parser;
 use std::fs::{File, OpenOptions};
 use std::io::{BufRead, BufReader, BufWriter, Write};
@@ -20,21 +21,40 @@ pub struct Opts {
     #[clap(short = 'p', long)]
     pub path: String,
 
-    #[clap(short = 'o', long, default_value = "out.log")]
-    pub output: String,
+    #[clap(short = 'o', long)]
+    pub output: Option<String>,
+}
+
+fn default_log_name() -> String {
+    format!("temp_{}.log", Utc::now().format("%Y-%m-%d_%H-%M-%S"))
 }
 
 fn main() -> anyhow::Result<()> {
     let opts: Opts = Opts::parse();
 
-    let in_file = File::open(opts.path)?;
+    let in_file = File::open(&opts.path)?;
     let reader = BufReader::new(in_file);
 
+    let out_file_name = opts.output.clone().unwrap_or(default_log_name());
+    process_and_save(&out_file_name, reader, &opts)?;
+
+    if opts.output.is_none() {
+        std::fs::rename(out_file_name, &opts.path)?;
+    }
+
+    Ok(())
+}
+
+fn process_and_save(
+    out_file_name: &str,
+    reader: BufReader<File>,
+    opts: &Opts,
+) -> anyhow::Result<()> {
     let out_file = OpenOptions::new()
         .create(true)
         .write(true)
         .truncate(true)
-        .open(opts.output)?;
+        .open(out_file_name)?;
     let mut writer = BufWriter::new(out_file);
 
     let mut flush_index = 0;
@@ -52,6 +72,5 @@ fn main() -> anyhow::Result<()> {
     if flush_index != 0 {
         writer.flush()?;
     }
-
     Ok(())
 }
